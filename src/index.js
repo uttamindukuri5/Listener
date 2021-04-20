c28b99852c13dfc5bf46b813efa5dbd0294622cf
uttamindukuri <uttamindukuri5@gmail.com>
2021-04-20 17:04:20 -0500

    added logic for determining ip to user for file name mapping

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ const fs = require('fs');
 const http = require('http');
 const queryString = require('querystring');
 
-const server = http.createServer((req, res) => {
+const IP_FILE_NAME = 'ip-mapping.json';
+let fileName;
+
+const server = http.createServer(async (req, res) => {
+
     if (req.method === 'POST') {
         let rawData = '';
+
         req.on('data', chunk => {
             rawData += chunk;
         });
@@ -12,7 +17,7 @@ const server = http.createServer((req, res) => {
         req.on('end', async () => {
             const parseData = queryString.decode(rawData);
             console.log('PARSE DATA: ', parseData['<?xml version']);
-            fs.writeFile(`${__dirname}/data/test-${new Date()}.xml`, `<?xml version=${ parseData['<?xml version'] }`, err => {
+            fs.writeFile(`${__dirname}/data/${fileName}-${new Date()}.xml`, `<?xml version=${ parseData['<?xml version'] }`, err => {
                 if (err) {
                     throw err;
                 }
@@ -22,6 +27,20 @@ const server = http.createServer((req, res) => {
     }
 
     res.end('Hello');
+});
+
+server.on('connection', sock => {
+    const rawIp = fs.readFileSync(`${__dirname}/${IP_FILE_NAME}`);
+    const ipList = JSON.parse(rawIp);
+    console.log(ipList);
+    const ip = sock.remoteAddress;
+    if (ipList[ip]) {
+        fileName = ipList[ip];
+    } else {
+        fileName = ip;
+    }
+    console.log('SOCK: ', sock.remoteAddress);
+
 });
 
 server.listen(3000, '0.0.0.0', () => {
